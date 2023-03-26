106d017f15e3f483a755bb015e8ce51413992b22
CF <cfreitas12345@gmail.com>
2023-03-25 21:12:42 -0700

    Removed .EXAMPLE, added some routes & the index.js.

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
+  // Define the columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Primary key in SQL is a single, or a group of fields or columns that can
+      // identify a row in a table. Putting it simply, it is a column that accepts
+      // unique values for each row. Thus, whenever you use the INSERT INTO command 
+      // to insert new values in a table, the value for the primary key column or 
+      // columns needs to be unique.
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Refer to 13 ORM: Lesson 24 for help.
+Product.belongsTo(Category, {
+  // A FOREIGN KEY is a field (or collection of fields) 
+  // in one table, that refers to the PRIMARY KEY in another 
+  // table. The table with the foreign key is called the child table, 
+  // & the table with the primary key is called the referenced or parent table.
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
